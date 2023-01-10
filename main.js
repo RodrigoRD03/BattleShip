57930941a04c6ae204bc17c9517debff707089e2
ROdrigoRRRRD <roddrigo2003cod@gmail.com>
2023-01-10 14:09:04 -0600

    paint

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 let section = document.querySelector('.content_Spaces');
-
-for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-        section.innerHTML += `<div class='drawer'></div>`
-        
+let draws;
+for (let i = 0; i < 20; i++) {
+    for (let j = 0; j < 20; j++) {
+        section.innerHTML += `<div class='drawer' id="C${j}-R${i}"></div>`;
     }
-    section.innerHTML += `<br>`
-    
 }
+draws = document.querySelectorAll('.drawer');
+
+
+document.querySelectorAll('.drawer').forEach(el => {
+    el.addEventListener('click', e => {
+        let id = e.target.getAttribute("id");
+        let element = document.querySelector("#"+id);
+        element.classList.toggle("paint");
+    });
+});
