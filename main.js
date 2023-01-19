82d28f90cc17aaa61f6b231174e6988ebf50f72c
ROdrigoRRRRD <roddrigo2003cod@gmail.com>
2023-01-19 17:45:27 -0600

    S

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
-let section = document.querySelector('.content_Spaces');
-let draws;
-for (let i = 0; i < 20; i++) {
-    for (let j = 0; j < 20; j++) {
-        section.innerHTML += `<div class='drawer' id="C${j}-R${i}"></div>`;
+let player1 = '.content_SpacesPlayer1';
+let player2 = '.content_SpacesPlayer2';
+class Players {
+    constructor(content, player){
+        this.content = content;
+        this.player = player;
+    }
+    
+    pintarElmentos(){
+        let section = document.querySelector(this.content);
+        let draws;
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 11; j++) {
+                section.innerHTML += `<div class='drawer' id="${this.player}-C${j}-R${i}"></div>`;
+            }
+        }
     }
 }
+
+
+let barcos1 = new Players(player1, "P1");
+let barcos2 = new Players(player2, "P2");
+
+barcos1.pintarElmentos();
+barcos2.pintarElmentos();
+
+
+let draws;
 draws = document.querySelectorAll('.drawer');
 
 
 document.querySelectorAll('.drawer').forEach(el => {
     el.addEventListener('click', e => {
         let id = e.target.getAttribute("id");
-        let element = document.querySelector("#"+id);
+        let element = document.querySelector("#" + id);
         element.classList.toggle("paint");
     });
 });
+
